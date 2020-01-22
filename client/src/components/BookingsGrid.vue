20457f734d0d4fd8ebf5bc24ad77536e6e98d835
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2020-01-22 12:04:38 +0000

    seeds file works, we have ID's (YAY)

---
+++ client/src/components/BookingsGrid.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+"name": "bookings-grid",
+data () {
+return {
+    bookings: []
+}
+},
+methods: {
+    fetchData(){
+        BookingsService.getBookings()
+        .then(bookings=> this.bookings = bookings)
+    }
+}
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ server/db/seeds.js
@@ -0,0 +1,26 @@
+use bookings;
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        name: "Jamie",
+        email: "jamie@codeclan.com",
+        status: "Booked"
+    },
+    {
+        name: "Catherine",
+        email: "catherine@codeclan.com",
+        status: "Checked-In"
+    },
+    {
+        name: "Silvia",
+        email: "silvia@codeclan.com",
+        status: "Checked-Out"
+    },
+    {
+        name: "Bob",
+        email: "bob@codeclan.com",
+        status: "Cancelled"
+    }
+
+])
