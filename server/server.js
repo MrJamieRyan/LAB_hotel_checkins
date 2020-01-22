548bb2ea003e4536c6a73536d3ed905da0205b7f
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2020-01-22 11:04:37 +0000

    Setup server and router etc

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router');
+const cors = require('cors');
+
+app.use(cors())
+app.use(parser.json())
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if(err){
+        console.log(err);
+    }
+
+    const db = client.db('hotel_bookings')
+    const bookingsCollection = db.collection('bookings')
+    const bookingsRouter = createRouter (bookingsCollection)
+    app.use('/api/bookings', bookingsRouter);
+
+    app.listen(3000, function(){
+        console.log(`app listening on port ${this.address().port}`);
+    })
+}) 
